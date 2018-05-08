7f29c8ffae5700d55c646d8c4d8fa561c9d31b73
Satoshi Nagayasu <snaga@uptime.jp>
2018-05-08 10:55:48 +0900

    Add simple database test with PostgreSQL service image.

--- testHello.py
+++ testHello.py
@@ -1,10 +1,13 @@
 import unittest
 
-from hello import hello
+from hello import hello, hello_pg
 
 class TestHello(unittest.TestCase):
     def test_hello_001(self):
         self.assertEquals('hello', hello())
+
+    def test_hello_pg_001(self):
+        self.assertEquals('PostgreSQL 10', hello_pg()[:13])
 
 
 if __name__ == '__main__':
